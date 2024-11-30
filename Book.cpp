365d94a7cf09d4eca2923665b9840efd83f9a722
chetan <chetan.sq05@gmail.com>
2024-11-30 08:00:23 +0530

    30/11

--- Book.cpp
+++ Book.cpp
@@ -1,27 +1,42 @@
-#include<iostream>
+#include <iostream>
 
 using namespace std;
 
-class Book{
-    private:
+class Book
+{
+private:
     int bookno;
     char booktitle[20];
     float price;
-    float totalcost();
-    public:
-    void input(){
-        cout<<"Enter book no:";
-        cin>>bookno;
-        cout<<"Enter booktitle:";
-        cin>>booktitle;
-        cout<<"Enter book no:";
-        cin>>price;
+    int n;
+    float totalcost(int n, float price);
+
+public:
+    void input()
+    {
+        cout << "Enter book no:";
+        cin >> bookno;
+        cout << "Enter booktitle:";
+        cin >> booktitle;
+        cout << "Enter book price:";
+        cin >> price;
     }
-    void purchase(){
-        
+    void purchase()
+    {
+        cout << "Enter the no of coppies:";
+        cin >> n;
+        cout << "Total cost is:" << totalcost(n, price);
     }
 };
 
-int main(){
+float Book::totalcost(int n, float price)
+{
+    return price * n;
+}
 
+int main()
+{
+    Book s1;
+    s1.input();
+    s1.purchase();
 }
