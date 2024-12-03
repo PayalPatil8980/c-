a91dea5661afcef93ea9b3a6323ecccdc0e6b702
chetan <chetan.sq05@gmail.com>
2024-12-03 09:43:27 +0530

    3/12

--- functionOverloading.cpp
+++ functionOverloading.cpp
@@ -1,30 +1,36 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-class Hello{
-    public:
-    void test(){
-        cout<<"Default constructor"<<endl;
+class Hello
+{
+public:
+    void test()
+    {
+        cout << "Default constructor" << endl;
     }
-    void test(int a){
-        cout<<"Value of a: "<<a<<endl;
+    void test(int a)
+    {
+        cout << "Value of a: " << a << endl;
     }
-    void test(int a,int b){
-        cout<<"Value of a+b : "<<a+b<<endl;
+    void test(int a, int b)
+    {
+        cout << "Value of a+b : " << a + b << endl;
     }
-    void test(double a,double b){
-        cout<<"Value of a: "<<a+b<<endl;
+    void test(double a, double b)
+    {
+        cout << "Value of a: " << a + b << endl;
     }
-    void test(int a,double b,char c){
-        cout<<"Value of a: "<<a+b<<c;
+    void test(int a, double b, char c)
+    {
+        cout << "Value of a: " << a + b << c;
     }
 };
-int main(){
+int main()
+{
     Hello s1;
     s1.test();
     s1.test(10);
-    s1.test(20,50);
-    s1.test(89.67,56.54);
-    s1.test(50,65.0,'a');
-
+    s1.test(20, 50);
+    s1.test(89.67, 56.54);
+    s1.test(50, 65.0, 'a');
 }
