5d7a0fc39252615bc3b33d6933c499b15d86d962
PayalPatil8980 <sq.payal30@gmail.com>
2024-12-10 09:16:55 +0530

    Add files via upload

---
+++ salaryinherit.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+using namespace std;
+
+class Sal{
+    public:
+    int sal;
+    float gross,hra,da;
+};
+
+class Gross:public Sal{
+    public:
+    void input(){
+        cout<<"Enter the salary:";
+        cin>>sal;
+    }
+    void printdata(){
+        if(sal>1 && sal<=5000){
+            hra=sal*8/100;
+            da=sal*20/100;
+            gross=sal+hra+da;
+        }
+        else if(sal>5000 && sal<=10000){
+            hra=sal*12/100;
+            da=sal*30/100;
+            gross=sal+hra+da;
+        }
+        else if(sal>10000 && sal<=15000){
+            hra=sal*15/100;
+            da=sal*40/100;
+            gross=sal+hra+da;
+        }
+        else if(sal>=15000){
+            hra=sal*8/100;
+            da=sal*20/100;
+            gross=sal+hra+da;
+        }
+        else{
+            cout<<"salary not acceptable";
+        }
+        cout<<"gross salary of employee:"<<gross;
+    }
+};
+
+int main(){
+    Gross s1;
+    s1.input();
+    s1.printdata();
+}
---
+++ singleinherit.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+class Base{
+    int a=10;
+    public:
+    int b=20;
+    protected:
+    int c=30;
+};
+class Derive:public Base{
+    public:
+    void printdata(){
+        cout<<c<<endl;
+        cout<<b<<endl;
+    }
+};
+int main(){
+    Derive s;
+    s.printdata();
+    cout<<s.b;
+}
